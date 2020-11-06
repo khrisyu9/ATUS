327f61bee3242789870bd5ebdeacb75cf5bca0b9
khrisyu9 <khrisyu@umich.edu>
2020-11-06 10:39:22 -0500

    upload subset .Rmd files and modified extract_exercise_act.R

--- extract_exercise_act.R
+++ extract_exercise_act.R
@@ -1,7 +1,7 @@
 ###############################################################################
 ############## Collect Exercise Timing of Primary Sleep Respodants ##############
 ###############################################################################
-act_before_sleep1 = read.csv( "D:/ATUS/Processed_Data/act_duration.1hr.debug.csv", check.names=FALSE)
+act_before_sleep1 = read.csv("D:/ATUS/Processed_Data/act_duration.1hr.debug.csv", check.names=FALSE)
 exer.act = read.csv("D:/ATUS/Processed_Data/exercise_activity.csv")
 library(dplyr)
 sum_exer = rep(0,nrow(act_before_sleep1))
@@ -13,6 +13,25 @@ length(which(sum_exer != 0))
 library(GLDEX)
 sum_nonzero_exer = fun.zero.omit(sum_exer) 
 
+max(sum_exer)
+
+min(sum_exer)
+
+length(which(sum_exer > 0))
+
+library(survey)
+svymean(sum_exer)
+
+for (i in 1:length(sum_exer)){
+  if (sum_exer[i]<0){
+    sum_exer[i] = 0
+  }
+}
+
+length(which(sum_exer != 0))
+
 mean(sum_nonzero_exer)
 
-act_before_sleep1 = cbind(act_before_sleep1, sum_exer)
+exer_before_sleep1 = cbind(act_before_sleep1[1:3], sum_exer)
+
+write.csv(exer_before_sleep1, "D:/ATUS/Processed_Data/exer_before_sleep1.csv")
